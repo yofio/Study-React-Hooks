e0ed0929b6681718e3d108026a67c9642fb2ca48
yofio <65844842+yofio@users.noreply.github.com>
2021-07-10 14:54:07 +0900

    カスタムフックを使わないデータ取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,54 @@
 import "./styles.css";
+import { UserCard } from "./components/UseCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/userPropfile";
 
 export default function App() {
+  const [userProfile, setUserProfile] = useState<Array<UserProfile>>([]);
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchdate = () => {
+    setLoading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfile(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchdate}>データ取得</button>
+      <br />
+      {error ? (
+        <p>エラー</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfile.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
